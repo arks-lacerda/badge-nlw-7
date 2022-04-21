59c44c165aa9c8595a50ca2585aea811c0905a33
Arthur Lacerda <39713179+tucasrl@users.noreply.github.com>
2022-04-21 20:36:15 -0300

    badge-nlw-7

---
+++ assets/js/main.js
@@ -0,0 +1,33 @@
+const LinksSocialMedia = {
+  github: 'tucasrl',
+  youtube: 'channel/UCD9k5-H9HgKO3_QZvkMSrsQ',
+  facebook: 'arthur.lacerda.56',
+  instagram: 'arthur_arks',
+  twitter: 'arthur_srl'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+    li.children[0].href = `https://www.${social}.com/${LinksSocialMedia[social]}`
+    //alert(li.children[0].href)
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
